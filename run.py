627539be8a1e7cbbbcf5e285f674184e45390e54
Thephelpster <thephelpster1992@gmail.com>
2022-11-19 19:57:12 +0000

    add welcome screen to game

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#-------------------------------------------------------------------------------
+
+def welcome():
+    """
+    Add the welcome screen for the game.
+    Display game name and creator.
+    """
+    print(" ")
+    print("  ______              _     _     _           ")
+    print(" |   _  \            | |   | |   | |          ")
+    print(" |  (_)  |   ____    | |_  | |_  | |    ____  ")
+    print(" |   _   <  /  _  \  | __| | __| | |   / _  \ ")
+    print(" |  (_)  | |  (_)  | | |_  | |_  | |_ |   __/ ")
+    print(" |______/   \___/\_\ \___| \___| \___| \____| ")
+    print(" ")
+    print("     _____   ___       _                      ")
+    print("    /   __) |   |     (_)                     ")
+    print("    \   \   |   |___   _     ____      __     ")
+    print("     \   \  |   _   | | |   |  _  \  / __|    ")
+    print("    __\   \ |  | |  | | |_  | (_)  | \__ \    ")
+    print("   (______/ |__| |__| \___| |   __/  |___/    ")
+    print("                            |__|              ")
+    print(" ")
+    print("                         By Jamie Phelps      ")
+
+def main():
+    """
+    Run all program functions
+    """
+    welcome()
+main()
